ccf8107e5c8e320a517b502bba47b965290eaed4
xschen <xs0040@gmail.com>
2017-04-25 22:51:59 +0800

    fix the array index out of range for the shuffling

--- pyalgs/algorithms/commons/shuffling.py
+++ pyalgs/algorithms/commons/shuffling.py
@@ -7,5 +7,5 @@ class KnuthShuffle(object):
     @staticmethod
     def shuffle(a):
         for i in range(1, len(a)):
-            r = randint(0, i+1)
+            r = randint(0, i)
             exchange(a, r, i)
