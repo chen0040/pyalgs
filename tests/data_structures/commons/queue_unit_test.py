94117f2fcfb08a94bdfc387f2032929f74cbe545
xschen <xs0040@gmail.com>
2017-04-25 22:50:15 +0800

    unit test for array queue

--- tests/data_structures/commons/queue_unit_test.py
+++ tests/data_structures/commons/queue_unit_test.py
@@ -43,6 +43,24 @@ class QueueUnitTest(unittest.TestCase):
         self.assertEqual(30, queue.dequeue())
         self.assertTrue(queue.is_empty())
 
+    def test_ArrayQueue(self):
+        queue = LinkedListQueue()
+        queue.enqueue(10)
+        self.assertEqual(1, queue.size())
+        self.assertFalse(queue.is_empty())
+        queue.enqueue(20)
+        self.assertEqual(2, queue.size())
+        queue.enqueue(30)
+
+        print([i for i in queue.iterate()])
+
+        self.assertEqual(3, queue.size())
+        self.assertEqual(10, queue.dequeue())
+        self.assertEqual(2, queue.size())
+        self.assertEqual(20, queue.dequeue())
+        self.assertEqual(1, queue.size())
+        self.assertEqual(30, queue.dequeue())
+        self.assertTrue(queue.is_empty())
 
 if __name__ == '__main__':
     unittest.main()
