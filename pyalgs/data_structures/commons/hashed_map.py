bdb684904d5f23c49cde8af904389f21cb4882d9
xschen <xs0040@gmail.com>
2017-04-26 14:32:19 +0800

    Implement hashed map with linear probing

--- pyalgs/data_structures/commons/hashed_map.py
+++ pyalgs/data_structures/commons/hashed_map.py
@@ -55,7 +55,10 @@ class HashedMapWithSeparateChaining(HashedMap):
     id = None
     N = 0
 
-    def __init__(self):
+    def __init__(self, m=None):
+        if m is None:
+            m = 97
+        self.M = m
         self.id = [None] * self.M
 
     def hash(self, key):
@@ -68,16 +71,12 @@ class HashedMapWithSeparateChaining(HashedMap):
         return self.get(key) is not None
 
     def keys(self):
-        queue = Queue.create()
         for i in range(self.M):
-            self.collect_keys(i, queue)
-        return queue.iterate()
-
-    def collect_keys(self, i, queue):
-        x = self.id[i]
-        while x is not None:
-            queue.enqueue(x.key)
-            x = x.next_node
+            x = self.id[i]
+            while x is not None:
+                key = x.key
+                x = x.next_node
+                yield key
 
     def size(self):
         return self.N
@@ -124,3 +123,82 @@ class HashedMapWithSeparateChaining(HashedMap):
         self.id[i] = Node(key, value)
         self.id[i].next_node = old_first
         self.N += 1
+
+
+class HashedMapWithLinearProbing(HashedMap):
+
+    M = 97
+    id = None
+    N = 0
+
+    def __init__(self, m=None):
+        if m is None:
+            m = 97
+        self.M = m
+        self.id = [None] * self.M
+
+    def hash(self, key):
+        return (hash(key) & 0x7fffffff) % self.M
+
+    def contains_key(self, key):
+        return self.get(key) is not None
+
+    def is_empty(self):
+        return self.N == 0
+
+    def keys(self):
+        for i in range(self.M):
+            x = self.id[i]
+            if x is not None:
+                yield x.value
+
+    def size(self):
+        return self.N
+
+    def get(self, key):
+        i = self.hash(key)
+        for j in range(self.M):
+            k = (i + j) % self.M
+            x = self.id[k]
+            if x is None:
+                return None
+            if util.cmp(key, x.key) == 0:
+                return x.value
+
+    def delete(self, key):
+        i = self.hash(key)
+        for j in range(self.M):
+            k = (i + j) % self.M
+            x = self.id[k]
+            if x is None:
+                return None
+            if util.cmp(key, x.key) == 0:
+                self.id[k] = None
+                self.N -= 1
+                if self.N == self.M // 4:
+                    self.resize(self.M // 2)
+                return x.value
+
+    def put(self, key, value):
+        i = self.hash(key)
+        for j in range(self.M):
+            k = (i + j) % self.M
+            x = self.id[k]
+            if x is None:
+                self.id[k] = Node(key, value)
+                self.N += 1
+                if self.N == self.M // 2:
+                    self.resize(self.M * 2)
+                break
+            if util.cmp(x.key, key) == 0:
+                self.id[k].value = value
+                break
+
+    def resize(self, new_size):
+        clone = HashedMapWithLinearProbing(new_size)
+        for i in range(self.M):
+            x = self.id[i]
+            if x is not None:
+                clone.put(x.key, x.value)
+        self.M = clone.M
+        self.id = clone.id
--- tests/data_structures/commons/hashed_map_unit_test.py
+++ tests/data_structures/commons/hashed_map_unit_test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from pyalgs.data_structures.commons.hashed_map import HashedMapWithSeparateChaining, HashedMap
+from pyalgs.data_structures.commons.hashed_map import HashedMapWithSeparateChaining, HashedMap, \
+    HashedMapWithLinearProbing
 
 
 class HashedMapUnitTest(unittest.TestCase):
@@ -92,6 +93,49 @@ class HashedMapWithSeparateChainingUnitTest(unittest.TestCase):
             map.delete(str(i))
             self.assertFalse(map.contains_key(str(i)))
 
+class HashedMapWithLinearProbingUnitTest(unittest.TestCase):
+    def test_binarySearchTree(self):
+        map = HashedMapWithLinearProbing()
+
+        map.put("one", 1)
+        map.put("two", 2)
+        map.put("three", 3)
+        map.put("six", 6)
+        map.put("ten", 10)
+        map.put("ten", 10)
+
+        self.assertEqual(1, map.get("one"))
+        self.assertEqual(2, map.get("two"))
+        self.assertEqual(3, map.get("three"))
+
+        self.assertTrue(map.contains_key("one"))
+        self.assertTrue(map.contains_key("two"))
+
+        self.assertEqual(5, map.size())
+        self.assertFalse(map.is_empty())
+
+        map.delete("one")
+        self.assertFalse(map.contains_key("one"))
+        self.assertEqual(4, map.size())
+
+        map.delete("ten")
+        self.assertFalse(map.contains_key("ten"))
+        self.assertEqual(3, map.size())
+
+        map.delete("three")
+        self.assertFalse(map.contains_key("three"))
+        self.assertEqual(2, map.size())
+
+        for i in range(100):
+            map.put(str(i), i)
+            self.assertEqual(i, map.get(str(i)))
+
+        for key in map.keys():
+            print(key)
+
+        for i in range(100):
+            map.delete(str(i))
+            self.assertFalse(map.contains_key(str(i)))
 
 if __name__ == '__main__':
     unittest.main()
